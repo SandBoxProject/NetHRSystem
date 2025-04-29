18dc4fcaec18422edbdf9a48e0e5a3fa580c74bb
zakiiizakaria <zakihelmi1997@gmail.com>
2025-04-29 11:19:40 +0800

    Modify register API to use RegisterDTO for new register flow to create user and also employee based on user input

---
+++ DTOs/RegisterDto.cs
@@ -0,0 +1,13 @@
+namespace NetHRSystem.DTOs
+{
+    public class RegisterDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string JobTitle { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
